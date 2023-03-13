1e9bd83da12878d490a427825208fcc11a04f128
iHamza7 <khanazam3324@gmail.com>
2023-03-12 20:10:02 -0700

    lib changed

--- lib/getlaglog.dart
+++ lib/getlaglog.dart
@@ -1,1 +1,15 @@
+import 'package:flutter/material.dart';
 
+class GetLagLat extends StatefulWidget {
+  const GetLagLat({super.key});
+
+  @override
+  State<GetLagLat> createState() => _GetLagLatState();
+}
+
+class _GetLagLatState extends State<GetLagLat> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
